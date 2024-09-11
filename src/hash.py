cfae7b9a320d84ae7cd229ce17e707cebb131005
Vitor Meriat <vitormeriat@gmail.com>
2024-09-11 22:15:47 +0100

    Update to poetry

--- src/hash.py
+++ src/hash.py
@@ -4,7 +4,6 @@ import humanize
 text = string.ascii_uppercase * 100_000_000
 
 # Show the first 50 characters
-print(f"Show the first 50 characteres:\n{text[:50]}")  
+print(f"Show the first 50 characteres:\n{text[:50]}")
 
 print(f"{len(text)} > {humanize.intword(len(text))}")
-
